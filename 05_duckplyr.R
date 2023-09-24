2eb6d13ca67fc96f4685022d6840cfc7014817c8
Javier Orraca <orraca.javier@gmail.com>
2023-09-24 10:20:57 -0700

    normalizing dplyr pipeline across methods

--- 05_duckplyr.R
+++ 05_duckplyr.R
@@ -21,52 +21,30 @@ bnch <- bench::mark(
   min_iterations = 50,
   tibble_to_arrow = nyc_taxi_tibble |> 
     arrow::as_arrow_table() |> 
+    dplyr::filter(passenger_count > 1) |> 
     dplyr::group_by(year) |> 
     dplyr::summarise(
       all_trips = n(),
-      shared_trips = sum(passenger_count > 1, na.rm = T)
+      shared_trips = sum(passenger_count, na.rm = T)
     ) |>
     dplyr::mutate(pct_shared = shared_trips / all_trips * 100) |> 
     dplyr::collect(),
   tibble_to_duckplyr = nyc_taxi_tibble |> 
     duckplyr::as_duckplyr_df() |> 
+    duckplyr::filter(passenger_count > 1) |> 
     duckplyr::summarise(
       all_trips = n(),
-      shared_trips = sum(passenger_count > 1, na.rm = T),
+      shared_trips = sum(passenger_count, na.rm = T),
       .by = year
       ) |>
     duckplyr::mutate(pct_shared = shared_trips / all_trips * 100),
   tibble_to_dplyr = nyc_taxi_tibble |> 
+    dplyr::filter(passenger_count > 1) |> 
     dplyr::group_by(year) |> 
     dplyr::summarise(all_trips = n(),
-                     shared_trips = sum(passenger_count > 1, na.rm = T)) |>
+                     shared_trips = sum(passenger_count, na.rm = T)) |>
     dplyr::mutate(pct_shared = shared_trips / all_trips * 100),
   arrow_table = nyc_taxi |> 
-    dplyr::group_by(year) |> 
-    dplyr::summarise(
-      all_trips = n(),
-      shared_trips = sum(passenger_count > 1, na.rm = T)
-    ) |>
-    dplyr::mutate(pct_shared = shared_trips / all_trips * 100) |> 
-    dplyr::collect(),
-  duckplyr_df = nyc_taxi_duckplyr_df |> 
-    duckplyr::summarise(
-      all_trips = n(),
-      shared_trips = sum(passenger_count > 1, na.rm = T),
-      .by = year
-    ) |>
-    duckplyr::mutate(pct_shared = shared_trips / all_trips * 100),
-  check = FALSE
-)
-toc()
-
-autoplot(bnch)
-
-tic()
-bnch_2 <- bench::mark(
-  min_iterations = 50,
-  tibble_to_arrow = nyc_taxi_tibble |> 
-    arrow::as_arrow_table() |> 
     dplyr::filter(passenger_count > 1) |> 
     dplyr::group_by(year) |> 
     dplyr::summarise(
@@ -75,16 +53,7 @@ bnch_2 <- bench::mark(
     ) |>
     dplyr::mutate(pct_shared = shared_trips / all_trips * 100) |> 
     dplyr::collect(),
-  arrow_table = nyc_taxi |> 
-    dplyr::filter(passenger_count > 1) |> 
-    dplyr::group_by(year) |> 
-    dplyr::summarise(
-      all_trips = n(),
-      shared_trips = sum(passenger_count, na.rm = T)
-    ) |>
-    dplyr::mutate(pct_shared = shared_trips / all_trips * 100) |> 
-    dplyr::collect(),
-  arrow_table_to_duckdb = nyc_taxi |> 
+  arrow_to_duckdb = nyc_taxi |> 
     arrow::to_duckdb() |> 
     dplyr::filter(passenger_count > 1) |> 
     dplyr::group_by(year) |> 
@@ -94,8 +63,17 @@ bnch_2 <- bench::mark(
     ) |>
     dplyr::mutate(pct_shared = shared_trips / all_trips * 100) |> 
     dplyr::collect(),
+  duckplyr_df = nyc_taxi_duckplyr_df |> 
+    duckplyr::filter(passenger_count > 1) |> 
+    duckplyr::summarise(
+      all_trips = n(),
+      shared_trips = sum(passenger_count, na.rm = T),
+      .by = year
+    ) |>
+    duckplyr::mutate(pct_shared = shared_trips / all_trips * 100),
   check = FALSE
 )
 toc()
 
-autoplot(bnch_2)
+autoplot(bnch)
+
